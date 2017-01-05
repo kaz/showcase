f8243d2daaffedd9713e80016bb74191688948f4
Kazuki Sawada <kazuki@6715.jp>
2017-01-05 11:25:45 +0900

    Dynamic deployment

--- docker/setup.sh
+++ docker/setup.sh
@@ -1,34 +1,45 @@
+# Network
 docker network create traplus
 
+# Build system
 docker build -t system/mariadb system/mariadb
 docker build -t system/mongodb system/mongodb
 docker build -t system/phpfpm system/phpfpm
 docker build -t system/nginx system/nginx
 
+# Run system
+
 docker run -dit \
 --name mariadb \
 --volume `pwd`/../data/mariadb:/var/lib/mysql \
 --network traplus \
+--restart always \
 system/mariadb
 
 docker run -dit \
 --name mongodb \
 --volume `pwd`/../data/mongodb:/var/lib/mongodb \
 --network traplus \
+--restart always \
 system/mongodb
 
 docker run -dit \
 --name phpfpm \
 --volume `pwd`/../data/repositories:/srv \
 --network traplus \
+--restart always \
 system/phpfpm
 
 docker run -dit \
 --name nginx \
 --volume `pwd`/../data/nginx:/etc/nginx/conf.d \
 --volume `pwd`/../data/repositories:/srv \
---network traplus \
 --publish 8080:80 \
+--network traplus \
+--restart always \
 system/nginx
 
-docker rm -f nginx phpfpm mariadb mongodb
+# Build runtime
+docker build -t runtime/node runtime/node
+
+
