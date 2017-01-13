41e9c0e217bd0e8a71228dacbcdeafbaea1b6749
Kazuki Sawada <kazuki@6715.jp>
2017-01-13 22:50:44 +0900

    Log / rm app

--- index.js
+++ index.js
@@ -4,8 +4,18 @@ const Logger = require("./lib/logger");
 const deployer = require("./lib/deployer");
 const connector = require("./lib/connector");
 
-const conn = connector.new();
-deployer(conn, "root/test").catch(error => {
-	Logger.exception(error);
-	console.trace(error);
-}).then(_ => conn.close());
+(async _ => {
+	const repo = "root/test";
+	const conn = connector.new();
+	if(0){
+		const result = await deployer.run(conn, repo).catch(error => Logger.exception(error));
+		if(result !== true){
+			await deployer.cancel(repo);
+		}
+		await deployer.save(conn, repo);
+		await deployer.saveLog(conn, repo, Logger.get());
+	}else{
+		await deployer.remove(conn, repo);
+	}
+	conn.close();
+})().catch(error => console.trace(error));
