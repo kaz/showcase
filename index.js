760b4541e9a0be5f51702ac32174f8bcceb939c5
Kazuki Sawada <kazuki@6715.jp>
2017-01-14 20:53:22 +0900

    Add model

--- index.js
+++ index.js
@@ -7,13 +7,12 @@ const connector = require("./lib/connector");
 (async _ => {
 	const repo = "root/test";
 	const conn = connector.new();
-	if(0){
+	if(1){
 		const result = await deployer.run(conn, repo).catch(error => Logger.exception(error));
 		if(result !== true){
 			await deployer.cancel(repo);
 		}
-		await deployer.save(conn, repo);
-		await deployer.saveLog(conn, repo, Logger.get());
+		await deployer.saveRecord(conn, repo, Logger.get());
 	}else{
 		await deployer.remove(conn, repo);
 	}
