c6cb7f751eece3e3eca2d2e7fcfeee2d0912f11c
Kazuki Sawada <kazuki@6715.jp>
2017-01-18 18:07:45 +0900

    Catch webhook

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 "use strict";
 
+const utils = require("./lib/helper/utils");
 const Logger = require("./lib/helper/logger");
 const Deployer = require("./lib/helper/deployer");
 const Connector = require("./lib/helper/connector");
+
 const AppModel = require("./lib/model/app");
 
 (async _ => {
-	const app = new AppModel();
-	await app.resolve("root/test", "master");
+	await Deployer.setupNginxSystem();
 	
-	if(1){
+	const app = new AppModel();
+	await app.resolve("kaz/m", "master");
+	
+	if(0){
 		await Deployer.run(app);
 	}else{
 		await Deployer.remove(app);
