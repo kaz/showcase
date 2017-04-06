5f355695c94f757d0d8d95ccee962096d014b996
Kazuki Sawada <kazuki@6715.jp>
2017-04-06 23:22:38 +0900

    Caching token parsing result

--- docker/system/nginx/verify.lua
+++ docker/system/nginx/verify.lua
@@ -1,8 +1,14 @@
 local token = ngx.var.cookie_traP_ext_token or ""
 
-local handle = io.popen("/etc/nginx/verify '" .. string.gsub(token, "'", "") .. "'")
-local user = handle:read("*all")
-handle:close()
+local token_store = ngx.shared.token_store
+local user = token_store:get(token)
+
+if user == nil then
+	local handle = io.popen("/etc/nginx/verify '" .. string.gsub(token, "'", "") .. "'")
+	local user = handle:read("*all")
+	handle:close()
+	token_store:set(token, user)
+end
 
 ngx.req.clear_header("X-Showcase-User")
 
