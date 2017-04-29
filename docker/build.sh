f050dddd915cad0c766df1ece301d35ed5adf25b
Kazuki Sawada <kazuki@6715.jp>
2017-04-29 20:28:01 +0900

    WIP: 1.3.0
    All runtime images now merged into docker image named “runtime”

--- docker/build.sh
+++ docker/build.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
 
 # Build runtime
-docker build -t runtime/go runtime/go
-docker build -t runtime/java runtime/java
-docker build -t runtime/node runtime/node
-docker build -t runtime/php runtime/php
-docker build -t runtime/rust runtime/rust
+docker build -t runtime runtime
 
 # Prepare for building system
-docker run --rm --volume `pwd`/system/nginx:/work runtime/go bash /work/build.sh
+docker run --rm --volume `pwd`/system/nginx:/work runtime bash /work/build.sh
 
 # Build system
 docker build -t system/mariadb system/mariadb
--- docker/system/nginx/build.sh
+++ docker/system/nginx/build.sh
@@ -10,21 +10,20 @@ go get github.com/dgrijalva/jwt-go
 go build -o /work/build/verify
 
 # patch makepkg to run as root
-sed -i "s/EUID == 0/EUID == 1/g" /usr/bin/makepkg
+sed -i "s/EUID == 0/0/g" /usr/bin/makepkg
 
 # get PKGBUILD
 cd /tmp
-curl https://aur.archlinux.org/cgit/aur.git/snapshot/openresty.tar.gz | tar zxvf -
-curl https://aur.archlinux.org/cgit/aur.git/snapshot/openresty_luarocks.tar.gz | tar zxvf -
+git clone https://aur.archlinux.org/openresty.git
+git clone https://aur.archlinux.org/openresty_luarocks.git
 
 # make OpenResty
 cd /tmp/openresty
-makepkg --noconfirm --syncdeps --skippgpcheck
-pacman -U --noconfirm *.pkg.tar.xz
+cp -f /work/PKGBUILD .
+makepkg --noconfirm --syncdeps --install
 mv *.pkg.tar.xz /work/build
 
 # make LuaRocks for OpenResty
 cd /tmp/openresty_luarocks
-makepkg --noconfirm --syncdeps --skippgpcheck
-pacman -U --noconfirm *.pkg.tar.xz
+makepkg --noconfirm --syncdeps --install
 mv *.pkg.tar.xz /work/build
