b02e05c43fa13998947089ee0a2ccb0083c45540
Kazuki Sawada <kazuki@6715.jp>
2018-03-25 21:41:34 +0900

    update deps

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,12 @@
 "use strict";
 
 const gulp = require("gulp");
-const rimraf = require("rimraf");
-const closure = require("gulp-closure-compiler-service");
+const babel = require("gulp-babel");
+const uglify = require("gulp-uglify");
 
-gulp.task("clean", rimraf.bind(null, "client/js/min"));
-gulp.task("minify", callback => gulp.src(["client/js/*.js"]).pipe(closure()).pipe(gulp.dest("client/js/min")));
-
-gulp.task("default", ["minify"]);
+gulp.task("default", _ =>
+	gulp.src(["client/js/*.js"]).
+	pipe(babel({presets: ["env"]})).
+	pipe(uglify()).
+	pipe(gulp.dest("client/js/min"))
+);
